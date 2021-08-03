425bbd96acced20296f9900060434756b7e7a523
rohan-av <rohanaryavarma@gmail.com>
2021-08-03 20:26:02 +0800

    Get Assignment.js ready for rework

--- frontend/src/components/studentUser/Assignment.js
+++ frontend/src/components/studentUser/Assignment.js
@@ -2,37 +2,36 @@ import { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import { Auth } from '../../context/authContext';
 
-/*
-Assignment consists of:
-  - Question Dropdown
-  - Question Text
-  - Submission Card
-    - File Upload
-    - Text Input
-    - Submit Button       
-
-Data required:
-  - QuestionIDs
-
-Data retrieved:
-  - Question text
-
-Data submitted:
-  - Submission file / submission content
+/** 
+ * Assignment consists of:
+ *  - Question Dropdown
+ *  - Question Text
+ *  - Submission Card
+ *    - File Upload
+ *    - Text Input
+ *    - Submit Button       
+ *
+ * Data required:
+ *  - Student ID of User
+ *
+ * Data retrieved:
+ *  - Question IDs of questions open for submission
+ *  - Question text
+ *
+ * Data submitted:
+ *  - Submission file / submission content
 */
 class Assignment extends Component {
   constructor(props) {
     super(props);
     this.submitDocument = this.submitDocument.bind(this);
     this.state = {
-      questionId: null,
+      questionIds: [],
       submitted: false
     };
   }
 
-  submitDocument() {
-
-  }
+  submitDocument() { }
 
   render() {
     // const submitButton = <Button variant="contained" size="large" onClick={this.submitDocument}>Submit</Button>;
