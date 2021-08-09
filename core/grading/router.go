bee572ac1c73b0d7548cafa1647502c763ea00cf
Mohammad Yousuf Minhaj Zia <yousuf@glints.com>
2021-08-09 16:50:32 +0530

    added get routes for grades

--- core/grading/router.go
+++ core/grading/router.go
@@ -15,7 +15,10 @@ type GradingRoute struct {
 }
 
 func (gr GradingRoute) CreateRouters(route *mux.Router) {
-	gr.CreatePrivilegedRouter(route.PathPrefix("/grade").Subrouter())
+	gr.CreatePrivilegedRouter(route.NewRoute().Subrouter())
+	gr.GetGradesForMarkee(route.PathPrefix("/marker").Subrouter())
+	gr.GetGradesForMarkee(route.PathPrefix("/markee").Subrouter())
+	gr.GetGradesForMarkee(route.NewRoute().Subrouter())
 }
 
 func (gr GradingRoute) CreatePrivilegedRouter(route *mux.Router) {
@@ -33,4 +36,36 @@ func (gr GradingRoute) CreateGradeRouter(route *mux.Router) {
 		route.Use(utils.MarkerCheckMiddleware(gr.DB, "grade", "claims"))
 	}
 	route.HandleFunc("", utils.DBCreateHandleFunc(gr.DB, &models.Grade{}, "grade", true)).Methods(http.MethodPost)
+}
+
+func (gr GradingRoute) GetGradesForMarkee(route *mux.Router) {
+	route.Use(utils.DecodeBodyMiddleware(&models.Grade{}, "grade"))
+	if os.Getenv("RUN_ENV") == "production" {
+		route.Use(utils.ValidateJWTMiddleware("Student", "claims"))
+		route.Use(utils.EnrollmentCheckMiddleware(gr.DB, "claims", "moduleId"))
+		route.Use(utils.MarkeeCheckMiddleware(gr.DB, "grade", "claims"))
+	}
+
+	route.HandleFunc("", utils.DBGetFromData(gr.DB, &models.Grade{}, "grade", &[]models.Grade{})).Methods(http.MethodGet)
+}
+
+func (gr GradingRoute) GetGradesForMarker(route *mux.Router) {
+	route.Use(utils.DecodeBodyMiddleware(&models.Grade{}, "grade"))
+	if os.Getenv("RUN_ENV") == "production" {
+		route.Use(utils.ValidateJWTMiddleware("Student", "claims"))
+		route.Use(utils.EnrollmentCheckMiddleware(gr.DB, "claims", "moduleId"))
+		route.Use(utils.MarkerCheckMiddleware(gr.DB, "grade", "claims"))
+	}
+
+	route.HandleFunc("", utils.DBGetFromData(gr.DB, &models.Grade{}, "grade", &[]models.Grade{})).Methods(http.MethodGet)
+}
+
+func (gr GradingRoute) GetGradesForStaff(route *mux.Router) {
+	route.Use(utils.DecodeBodyMiddleware(&models.Grade{}, "grade"))
+	if os.Getenv("RUN_ENV") == "production" {
+		route.Use(utils.ValidateJWTMiddleware("Staff", "claims"))
+		route.Use(utils.SupervisionCheckMiddleware(gr.DB, "claims", "moduleId"))
+	}
+
+	route.HandleFunc("", utils.DBGetFromData(gr.DB, &models.Grade{}, "grade", &[]models.Grade{})).Methods(http.MethodGet)
 }
