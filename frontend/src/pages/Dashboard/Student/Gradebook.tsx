9ae16dcf1292ac1a89671bae1299e500618bc1e0
Mohammad Yousuf Minhaj Zia <yousuf@glints.com>
2021-09-15 15:28:55 +0530

    frontend: updated GradeBook with Annotator

--- frontend/src/pages/Dashboard/Student/Gradebook.tsx
+++ frontend/src/pages/Dashboard/Student/Gradebook.tsx
@@ -12,8 +12,10 @@ import {
   Typography,
 } from "@material-ui/core";
 import { FC, useContext, useEffect, useRef, useState } from "react";
+import { IHighlight } from "react-pdf-highlighter";
 import { useHistory, useRouteMatch } from "react-router-dom";
 import {
+  downloadSubmission,
   getGradesForMarker,
   getGradesForStudent,
   getPairings,
@@ -23,6 +25,7 @@ import {
   uploadSubmission,
 } from "../../../actions/moduleActions";
 import { ButtonAppBar, Page } from "../../../components/NavBar";
+import { Annotator } from "../../../components/PdfViewer";
 import {
   StyledTableCell,
   StyledTableContainer,
@@ -64,6 +67,10 @@ export const Gradebook: FC<{
   const [pairings, setPairings] = useState<Pagination<Pairing>>({});
   const [rubrics, setRubrics] = useState<Pagination<Rubric>>({});
   const [grades, setGrades] = useState<Map<number, Grade>>(null);
+  const [highlights, setHighlights] = useState<Array<IHighlight>>(
+    new Array<IHighlight>()
+  );
+  const [downloadURL, setDownloadURL] = useState<string>(null);
 
   const hiddenFileInput = useRef(null);
 
@@ -86,70 +93,114 @@ export const Gradebook: FC<{
     });
   };
 
+  const handleDownload = async () => {
+    try {
+      setDownloadURL(
+        await downloadSubmission(moduleId, questionId, state.user.ID)
+      );
+    } catch (e) {
+      alert("No submission found");
+      console.error(e);
+    }
+  };
+
   return (
     <div>
-      <Select
-        name="status"
-        onChange={(e) => {
-          setStudent(e.target.value as number);
-          setGrades(null);
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        spacing={1}
+        style={{
+          marginBottom: "10px",
         }}
       >
-        {pairings?.rows?.map((pair, key) => {
-          return (
-            <MenuItem key={key} value={pair.ID}>{`Student ${key + 1
-              }`}</MenuItem>
-          );
-        })}
-      </Select>
-      <StyledTableContainer>
-        <StyledTableHead>
-          <StyledTableCell>ID</StyledTableCell>
-          <StyledTableCell>Criteria</StyledTableCell>
-          <StyledTableCell>Description</StyledTableCell>
-          <StyledTableCell>Min</StyledTableCell>
-          <StyledTableCell>Max</StyledTableCell>
-          {grades && (
-            <>
-              <StyledTableCell>Grade</StyledTableCell>
-              <StyledTableCell>Comment</StyledTableCell>
-            </>
-          )}
-        </StyledTableHead>
-        <TableBody>
-          {rubrics.rows?.map((rubric) => {
-            return (
-              <StyledTableRow hover={true} key={rubric.ID}>
-                <StyledTableCell component="th" scope="row">
-                  {rubric.ID}
-                </StyledTableCell>
-                <StyledTableCell component="th" scope="row">
-                  {rubric.Criteria}
-                </StyledTableCell>
-                <StyledTableCell component="th" scope="row">
-                  {rubric.Description}
-                </StyledTableCell>
-                <StyledTableCell component="th" scope="row">
-                  {rubric.MinMark}
-                </StyledTableCell>
-                <StyledTableCell component="th" scope="row">
-                  {rubric.MaxMark}
-                </StyledTableCell>
+        {downloadURL && (
+          <Grid item>
+            <Annotator
+              url={downloadURL}
+              setHighlights={setHighlights}
+              highlights={highlights}
+            />
+          </Grid>
+        )}
+        <Grid item>
+          <Grid
+            container
+            direction="column"
+            alignItems="center"
+            spacing={1}
+            style={{
+              marginBottom: "10px",
+            }}
+          >
+            <Select
+              name="status"
+              onChange={(e) => {
+                setStudent(e.target.value as number);
+                setGrades(null);
+                handleDownload();
+              }}
+            >
+              {pairings?.rows?.map((pair, key) => {
+                return (
+                  <MenuItem key={key} value={pair.ID}>{`Student ${
+                    key + 1
+                  }`}</MenuItem>
+                );
+              })}
+            </Select>
+            <StyledTableContainer>
+              <StyledTableHead>
+                <StyledTableCell>ID</StyledTableCell>
+                <StyledTableCell>Criteria</StyledTableCell>
+                <StyledTableCell>Description</StyledTableCell>
+                <StyledTableCell>Min</StyledTableCell>
+                <StyledTableCell>Max</StyledTableCell>
                 {grades && (
                   <>
-                    <StyledTableCell component="th" scope="row">
-                      {grades?.get(rubric.ID)?.Grade}
-                    </StyledTableCell>
-                    <StyledTableCell component="th" scope="row">
-                      {grades?.get(rubric.ID)?.Comment}
-                    </StyledTableCell>
+                    <StyledTableCell>Grade</StyledTableCell>
+                    <StyledTableCell>Comment</StyledTableCell>
                   </>
                 )}
-              </StyledTableRow>
-            );
-          })}
-        </TableBody>
-      </StyledTableContainer>
+              </StyledTableHead>
+              <TableBody>
+                {rubrics.rows?.map((rubric) => {
+                  return (
+                    <StyledTableRow hover={true} key={rubric.ID}>
+                      <StyledTableCell component="th" scope="row">
+                        {rubric.ID}
+                      </StyledTableCell>
+                      <StyledTableCell component="th" scope="row">
+                        {rubric.Criteria}
+                      </StyledTableCell>
+                      <StyledTableCell component="th" scope="row">
+                        {rubric.Description}
+                      </StyledTableCell>
+                      <StyledTableCell component="th" scope="row">
+                        {rubric.MinMark}
+                      </StyledTableCell>
+                      <StyledTableCell component="th" scope="row">
+                        {rubric.MaxMark}
+                      </StyledTableCell>
+                      {grades && (
+                        <>
+                          <StyledTableCell component="th" scope="row">
+                            {grades?.get(rubric.ID)?.Grade}
+                          </StyledTableCell>
+                          <StyledTableCell component="th" scope="row">
+                            {grades?.get(rubric.ID)?.Comment}
+                          </StyledTableCell>
+                        </>
+                      )}
+                    </StyledTableRow>
+                  );
+                })}
+              </TableBody>
+            </StyledTableContainer>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 };
