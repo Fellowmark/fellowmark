03ed0acade0e4aad7840e0414c00c2cb4fd35d13
Mohammad Yousuf Minhaj Zia <yzia2000@gmail.com>
2022-02-21 13:35:36 +0800

    frontend: implemented cookie consent notice

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-undef */
 import "./App.css";
 import axios from "axios";
 import Routes from "./Routes";
@@ -6,8 +7,17 @@ import { useHistory } from "react-router-dom";
 import { AuthContext, TimeoutContext } from "./context/context";
 import { authenticate } from "./utils/auth";
 import { logoutUser, setAuthorizationHeader } from "./actions/userActions";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@material-ui/core";
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL ?? 'http://localhost:5050/api';
+axios.defaults.baseURL =
+  process.env.REACT_APP_API_URL ?? "http://localhost:5050/api";
 
 const useAuthHook = () => {
   const { dispatch } = useContext(AuthContext);
@@ -18,23 +28,60 @@ const useAuthHook = () => {
     const secondsLeft = authenticate(dispatch);
     if (secondsLeft) {
       cancelTimeout();
-      createTimeout(setTimeout(() => {
-        logoutUser(history, dispatch);;
-        alert("Session expired");
-      }, secondsLeft - 30));
+      createTimeout(
+        setTimeout(() => {
+          logoutUser(history, dispatch);
+          alert("Session expired");
+        }, secondsLeft - 30)
+      );
       setAuthorizationHeader(localStorage.jwt);
     } else {
       logoutUser(history, dispatch);
       cancelTimeout();
     }
   }, []);
-}
+};
 
 function App() {
+  const [open, setOpen] = useState(true);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   useAuthHook();
 
   return (
     <div className="App container">
+      <>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{"Privacy Notice"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Agree This site uses cookies. By clicking accept or continuing to
+              use this site, you agree to our use of cookies. For more details,
+              please see our{" "}
+              <a href="https://www.nus.edu.sg/privacy-notice/">
+                Privacy Policy
+              </a>
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>
+              Agree
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </>
       <Routes />
     </div>
   );
