54307ff62cabd65e2622a81f01c9493cd8b426d4
LAPTOP-UM975EAE\Qi <tjq888@gmail.com>
2022-03-23 19:39:13 +0800

    update nav bar colour for admin

--- frontend/src/pages/Dashboard/Admin/Supervisors.tsx
+++ frontend/src/pages/Dashboard/Admin/Supervisors.tsx
@@ -52,7 +52,7 @@ export const Supervisors: FC = () => {
 
   return (
     <div>
-      <ButtonAppBar pageList={pageList} currentPage="Supervisors" username= {`${state?.user?.Name}`} colour='red'/>
+      <ButtonAppBar pageList={pageList} currentPage="Supervisors" username= {`${state?.user?.Name}`} colour='orange'/>
       <StyledTableContainer>
         <StyledTableHead>
           <StyledTableCell>ID</StyledTableCell>
